5512bca54616488b7806e6ef4dda25c00aebdab3
fazlerabbi13 <mohammad0132794@gmail.com>
2025-05-10 20:19:13 +0600

    sort an array

---
+++ SortArray.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class SortArray {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 7};
+        Arrays.sort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
