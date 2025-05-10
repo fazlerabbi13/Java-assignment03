2fc0275c60bcfc00074733613987fb55444fe9e6
fazlerabbi13 <mohammad0132794@gmail.com>
2025-05-10 20:24:53 +0600

    finding array length

---
+++ ArrayLength.java
@@ -0,0 +1,6 @@
+public class ArrayLength {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 7};
+        System.out.println("Length of array: " + arr.length);
+    }
+}
