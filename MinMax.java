b38000f453a66e5402708e79fa84b64bca744c33
fazlerabbi13 <mohammad0132794@gmail.com>
2025-05-10 20:29:53 +0600

    finding the maximum and minimum value from an array

---
+++ MinMax.java
@@ -0,0 +1,13 @@
+public class MinMax {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 7};
+        int max = arr[0];
+        int min = arr[0];
+        for (int num : arr) {
+            if (num > max) max = num;
+            if (num < min) min = num;
+        }
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
+    }
+}
